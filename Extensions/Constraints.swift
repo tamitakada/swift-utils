c19516f90116b590a82ea31ccb57ef294d7c18f2
Tami Takada <tamitakada@gmail.com>
2022-07-06 13:49:05 -0400

    added constraint extensions

---
+++ Extensions/Constraints.swift
@@ -0,0 +1,124 @@
+import UIKit
+
+
+extension UIView {
+    func centerView(x: Bool, y: Bool) {
+        self.translatesAutoresizingMaskIntoConstraints = false
+        
+        if x {
+            if let centerX = superview?.centerXAnchor {
+                self.centerXAnchor.constraint(equalTo: centerX).isActive = true
+            }
+        }
+        
+        if y {
+            if let centerY = superview?.centerYAnchor {
+                self.centerYAnchor.constraint(equalTo: centerY).isActive = true
+            }
+        }
+    }
+    
+    func constrain(
+        top: NSLayoutYAxisAnchor?, constantTop: CGFloat?,
+        bottom: NSLayoutYAxisAnchor?, constantBottom: CGFloat?,
+        trailing: NSLayoutXAxisAnchor?, constantTrailing: CGFloat?,
+        leading: NSLayoutXAxisAnchor?, constantLeading: CGFloat?) {
+        
+        self.translatesAutoresizingMaskIntoConstraints = false
+        
+        if let top = top {
+            self.topAnchor.constraint(equalTo: top, constant: constantTop ?? 0).isActive = true
+        }
+
+        if let bottom = bottom {
+            self.bottomAnchor.constraint(equalTo: bottom, constant: constantBottom ?? 0).isActive = true
+        }
+
+        if let trailing = trailing {
+            self.trailingAnchor.constraint(equalTo: trailing, constant: constantTrailing ?? 0).isActive = true
+        }
+
+        if let leading = leading {
+            self.leadingAnchor.constraint(equalTo: leading, constant: constantLeading ?? 0).isActive = true
+        }
+    }
+    
+    func setSize(width: CGFloat?, height: CGFloat?) {
+        self.translatesAutoresizingMaskIntoConstraints = false
+        
+        if let width = width {
+            self.widthAnchor.constraint(equalToConstant: width).isActive = true
+        }
+        
+        if let height = height {
+            self.heightAnchor.constraint(equalToConstant: height).isActive = true
+        }
+    }
+    
+    enum SizeType {
+        case width
+        case height
+        case both
+    }
+    
+    func setSize(view: UIView, multiplier: CGFloat, type: SizeType) {
+        self.translatesAutoresizingMaskIntoConstraints = false
+        
+        switch type {
+        case .width:
+            self.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: multiplier).isActive = true
+        case .height:
+            self.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: multiplier).isActive = true
+        case .both:
+            self.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: multiplier).isActive = true
+            self.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: multiplier).isActive = true
+        }
+    }
+    
+    func setSize(view: UIView, multiplier: CGFloat, type: SizeType, priority: UILayoutPriority) {
+        self.translatesAutoresizingMaskIntoConstraints = false
+        
+        let constraint: NSLayoutConstraint
+        var secondConstraint: NSLayoutConstraint?
+        
+        switch type {
+        case .width:
+            constraint = self.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: multiplier)
+        case .height:
+            constraint = self.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: multiplier)
+        case .both:
+            constraint = self.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: multiplier)
+            secondConstraint = self.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: multiplier)
+        }
+        
+        constraint.priority = priority
+        constraint.isActive = true
+        
+        secondConstraint?.priority = priority
+        secondConstraint?.isActive = true
+    }
+    
+    func setRectangularPadding(horizontal: CGFloat?, vertical: CGFloat?) {
+        if let side = horizontal {
+            constrain(
+                top: nil, constantTop: 0,
+                bottom: nil, constantBottom: 0,
+                trailing: superview?.trailingAnchor, constantTrailing: -side,
+                leading: superview?.leadingAnchor, constantLeading: side
+            )
+        }
+        
+        if let top = vertical {
+            constrain(
+                top: superview?.topAnchor, constantTop: top,
+                bottom: superview?.bottomAnchor, constantBottom: -top,
+                trailing: nil, constantTrailing: 0,
+                leading: nil, constantLeading: 0
+            )
+        }
+    }
+    
+    func fillView() {
+        setRectangularPadding(horizontal: 0, vertical: 0)
+    }
+}
